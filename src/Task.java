f91d7b06c93e66f4f13885e54d909b3671c2bf78
elyanilliranzocastro <yanil3500@gmail.com>
2018-11-28 16:11:04 -0500

    adds setters (all methods starting with edit; adds the addNote method as well as the deleteNote method

--- src/Task.java
+++ src/Task.java
@@ -9,4 +9,64 @@ public class Task {
     private ArrayList<Note> notes;
     private Metadata metadata;
 
+    public Task(String title){
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void editAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void editTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void editAssignedTo(String assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void editDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void editDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    public Metadata getMetadata() {
+        return metadata;
+    }
+
+    public void editMetadata(Metadata metadata) {
+        this.metadata = metadata;
+    }
+
+    public void addNote(String note){
+
+    }
+
+    public void deleteNote(int index){
+
+    }
+
 }
