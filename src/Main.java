04df4878d221c55f4e9651726eb2588519a968c7
elyanilliranzocastro <yanil3500@gmail.com>
2018-11-28 16:31:24 -0500

    The Main class extends JPanel; adds method stubs for KeyListener and MouseListener methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
-public class Main {
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Main extends JPanel implements KeyListener, MouseListener {
+
+    public static int WIDTH;
+    public static int HEIGHT;
+    Board board;
+
     public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
 
     }
 }
