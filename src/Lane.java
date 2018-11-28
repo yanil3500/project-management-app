d44455fa7b7265abdd6ecf07b8016ed0c6fcd8c1
elyanilliranzocastro <yanil3500@gmail.com>
2018-11-28 16:03:43 -0500

    adds Drawable interface.

---
+++ src/Lane.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Lane {
+    private ArrayList<Panel> panels;
+
+    public Lane() {
+        this.panels = new ArrayList<>();
+    }
+
+    public void addPanel(){
+
+    }
+}
