53a57ce2a53cd58885f5729911f76bb2dc83cb0c
elyanilliranzocastro <yanil3500@gmail.com>
2018-11-28 23:52:16 -0500

    makes Panel class implement the Drawable interface.

--- src/Panel.java
+++ src/Panel.java
@@ -1,12 +1,18 @@
-public class Panel {
+import java.awt.*;
+
+public class Panel implements Drawable {
     private Task task;
 
     public Panel(Task task) {
         this.task = task;
     }
 
-    public void movePanel(Lane lane){
+    public void movePanel(Lane lane) {
 
     }
 
+    @Override
+    public void draw(Graphics g) {
+
+    }
 }
