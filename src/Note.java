16141d8898017e63ad1f3d62a5b1dd862d9c7a0b
Elyanil Liranzo Castro <yanil3500@gmail.com>
2018-11-29 15:53:58 -0500

    adds Javadoc comments to the Note class; implements convenience constructor for providing note and the author of the note.

--- src/Note.java
+++ src/Note.java
@@ -1,7 +1,32 @@
+/**
+ * An instance of the <code>Note</code> class represents a note related to a specific task, and can provide more
+ * information about the nature/current status/etc. of the task.
+ */
 class Note {
 
+    /**
+     * The name of the note's creator. This field can be left blank.
+     */
     private String author;
+
+    /**
+     * The information the author wishes to leave about a task.
+     */
     private String text;
 
+    public Note(String text) {
+        this.text = text;
+    }
 
+    /**
+     * This is convenience constructor. If the author the note decides to provide their name, then this constructor will
+     * be used for creating note.
+     *
+     * @param author
+     * @param text
+     */
+    public Note(String author, String text) {
+        this.author = author;
+        this.text = text;
+    }
 }
