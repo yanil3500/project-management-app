fe95361f22e9d6cda0177fdd0c03d894053a93a3
Elyanil Liranzo Castro <yanil3500@gmail.com>
2018-11-28 23:55:21 -0500

    makes Lane class implement the Drawable interface.

--- src/Lane.java
+++ src/Lane.java
@@ -1,13 +1,20 @@
+import java.awt.*;
 import java.util.ArrayList;
 
-public class Lane {
+public class Lane implements Drawable {
+
     private ArrayList<Panel> panels;
 
     public Lane() {
         this.panels = new ArrayList<>();
     }
 
-    public void addPanel(){
+    public void addPanel() {
+
+    }
+
+    @Override
+    public void draw(Graphics g) {
 
     }
 }
