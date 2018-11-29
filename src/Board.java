a2960221447b1f30b2a4a88a438ca989a385c80c
Elyanil Liranzo Castro <yanil3500@gmail.com>
2018-11-28 23:45:56 -0500

    makes Board class implement the Drawable interface.

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,12 @@
-public class Board {
+import java.awt.*;
+
+public class Board implements Drawable {
     private Lane ToDo;
     private Lane InProgress;
     private Lane Completed;
+
+    @Override
+    public void draw(Graphics g) {
+
+    }
 }
