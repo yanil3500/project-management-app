d86bb4644c59ac3d263ab1e57ef5693b251f451c
Elyanil Liranzo Castro <yanil3500@gmail.com>
2018-11-28 23:44:19 -0500

    defines a copy constructor for Metadata class; Adds Javadoc comment to all of the Metadata methods and fields.

--- src/Metadata.java
+++ src/Metadata.java
@@ -3,25 +3,52 @@ import java.util.Date;
 
 class Metadata {
 
+    /**
+     * The date the Task was created.
+     */
     private Date dateCreated;
+
+    /**
+     * The date for the last time the Task was updated.
+     */
     private Date lastModified;
 
-    //Here is the Stackoverflow question that was looked for how to use the Calendar class.
+    //Here is the Stackoverflow question that was looked at for how to use the Calendar class.
     //Link: https://stackoverflow.com/questions/5165428/how-to-set-time-to-a-date-object-in-java
-    public Metadata(){
+    public Metadata() {
         this.dateCreated = Calendar.getInstance().getTime();
         this.lastModified = Calendar.getInstance().getTime();
     }
 
+    //Copy constructor
+    public Metadata(Metadata metadata) {
+        this.dateCreated = metadata.getDateCreated();
+        this.lastModified = metadata.getLastModified();
+    }
+
+
+    /**
+     * Gets the date the Task was created.
+     *
+     * @return a Date.
+     */
     public Date getDateCreated() {
         return new Date(dateCreated.getTime());
     }
 
+    /**
+     * Gets the date for the last time the Task was updated.
+     *
+     * @return a Date.
+     */
     public Date getLastModified() {
         return new Date(lastModified.getTime());
     }
 
-    public void updateLastModified(){
+    /**
+     * Updates lastModified to indicate the state of the Task has changed.
+     */
+    public void updateLastModified() {
         this.lastModified = new Date(System.nanoTime());
     }
 
