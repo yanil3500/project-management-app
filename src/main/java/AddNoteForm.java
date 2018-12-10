8b7ba0f4aecb1ca818daf8f669d178894bb5d265
elyanilliranzocastro <yanil3500@gmail.com>
2018-12-10 16:24:01 -0500

    adds buttons to panels

--- src/main/java/AddNoteForm.java
+++ src/main/java/AddNoteForm.java
@@ -1,11 +1,17 @@
 /*
  * The form was put together using the GUI Builder feature of the Netbeans IDE.
  */
+
 import javax.swing.*;
 
 
 public class AddNoteForm extends JDialog {
 
+    private JButton cancelButton;
+    private JButton okButton;
+    private JTextField taskNoteTextField;
+    private JLabel taskNoteTextFieldLabel;
+    private String note;
 
     /**
      * Creates new form AddNote
@@ -19,23 +25,24 @@ public class AddNoteForm extends JDialog {
 
     private void initComponents() {
 
-        taskNoteLabel = new JLabel();
-        taskNote = new JTextField();
+        taskNoteTextFieldLabel = new JLabel();
+        taskNoteTextField = new JTextField();
         okButton = new JButton();
         cancelButton = new JButton();
 
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Add a note");
 
-        taskNoteLabel.setText("Note:");
+        taskNoteTextFieldLabel.setText("Note:");
 
-        taskNote.setText("(Ex: Someone else has worked on this task.)");
+        taskNoteTextField.setText("(Ex: Someone else has worked on this task.)");
 
         okButton.setText("OK");
         okButton.addActionListener(evt -> {
-            this.note = taskNote.getText();
+            this.note = taskNoteTextField.getText();
             dispose();
         });
+
         //This Stackoverflow question was looked at: https://stackoverflow.com/questions/344969/making-a-jdialog-button-respond-to-the-enter-key
         //Makes "OK" button respond to the 'Enter' key.
         getRootPane().setDefaultButton(okButton);
@@ -49,9 +56,9 @@ public class AddNoteForm extends JDialog {
                 layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addComponent(taskNoteLabel)
+                                .addComponent(taskNoteTextFieldLabel)
                                 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(taskNote, GroupLayout.PREFERRED_SIZE, 345, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(taskNoteTextField, GroupLayout.PREFERRED_SIZE, 345, GroupLayout.PREFERRED_SIZE)
                                 .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGroup(layout.createSequentialGroup()
                                 .addGap(50, 50, 50)
@@ -65,8 +72,8 @@ public class AddNoteForm extends JDialog {
                         .addGroup(layout.createSequentialGroup()
                                 .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                        .addComponent(taskNoteLabel)
-                                        .addComponent(taskNote, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(taskNoteTextFieldLabel)
+                                        .addComponent(taskNoteTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
                                 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
                                 .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                                         .addComponent(okButton)
@@ -80,11 +87,5 @@ public class AddNoteForm extends JDialog {
     public String getNote() {
         return note;
     }
-
-    private JButton cancelButton;
-    private JButton okButton;
-    private JTextField taskNote;
-    private JLabel taskNoteLabel;
-    private String note;
 
 }
